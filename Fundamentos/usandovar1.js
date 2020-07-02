25167f9078d4900225a595b52536ddde4c2712ef
KingPack <llhendrek@gmail.com>
2020-07-01 23:52:02 -0400

    add letEmLoop.ja, VaremLoop.js, usandolet1.js,usandovar1.js, usandovar2.js

---
+++ Fundamentos/usandovar1.js
@@ -0,0 +1,17 @@
+{
+    {
+        {
+            {
+                 var sera = 'sera???'
+                
+            }
+        }
+    }
+}
+
+console.log(sera)
+
+function test () {
+    var local = 123
+    console.log(local)
+}
